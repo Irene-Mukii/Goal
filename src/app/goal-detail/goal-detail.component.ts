f0e31c8131011d06234fc510bf79f73179819f07
renniequin@gmail.com <renniequin@gmail.com>
2021-11-12 23:19:38 +0300

    still testing

---
+++ src/app/goal-detail/goal-detail.component.ts
@@ -0,0 +1,24 @@
+import { identifierModuleUrl } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {Goal} from '../goal';
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+  styleUrls: ['./goal-detail.component.css']
+})
+export class GoalDetailComponent implements OnInit {
+
+  @Input()
+  goal!: Goal;
+  @Output() isComplete = new EventEmitter()
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit('complete');
+  }
+  constructor() { }
+
+  ngOnInit(){
+  }
+
+}
---
+++ src/app/strikekethrough.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StrikethroughDirective } from './strikethrough.directive';
+
+describe('StrikekethroughDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StrikethroughDirective();
+    expect(directive).toBeTruthy();
+  });
+});
